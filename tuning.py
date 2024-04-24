79b61dd31fe44bf1e98c4b645c188cb4ca506c39
UltralyticsAssistant <web@ultralytics.com>
2024-04-24 06:38:38 +0000

    Auto-format by https://ultralytics.com/actions

--- tuning.py
+++ tuning.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+
 # Install and update Ultralytics and Ray Tune packages
 # pip install -U ultralytics "ray[tune]<=2.9.3"
 
@@ -9,7 +10,4 @@ from ultralytics import YOLO
 model = YOLO("yolov8custom.pt")
 
 # Run Ray Tune on the model
-result_grid = model.tune(data="bdd100k.yaml",
-                         space={"lr0": tune.uniform(1e-5, 1e-1)},
-                         epochs=50,
-                         use_ray=True)
+result_grid = model.tune(data="bdd100k.yaml", space={"lr0": tune.uniform(1e-5, 1e-1)}, epochs=50, use_ray=True)
