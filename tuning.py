2a9d63e58e2505ffb6cc4f63f6bc762c15d0aaeb
BAp3r <2152312121@qq.com>
2024-04-24 14:38:06 +0800

    modified:   tuning.py

--- tuning.py
+++ tuning.py
@@ -1,8 +1,15 @@
 from ultralytics import YOLO
+# Install and update Ultralytics and Ray Tune packages
+# pip install -U ultralytics "ray[tune]<=2.9.3"
 
-# 用自定义的数据集和结构从头训练
-# 导入结构yaml
+# # Optionally install W&B for logging
+# pip install wandb
 
+# Define a YOLO model
+model = YOLO("yolov8custom.pt")
 
-# model = YOLO('yolov8s.pt')
-model.tune(data="bdd100k.yaml", epochs=20, iterations=300, optimizer="Lion", plots=False, save=False, val=False)
+# Run Ray Tune on the model
+result_grid = model.tune(data="bdd100k.yaml",
+                         space={"lr0": tune.uniform(1e-5, 1e-1)},
+                         epochs=50,
+                         use_ray=True)
